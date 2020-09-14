82e17df194491c6ecef978ac7e99148f8b465af0
Eli <elimaynard923@gmail.com>
2020-09-14 00:40:51 -0700

    rename instructions

--- instrs.nim
+++ instrs.nim
@@ -28,17 +28,17 @@ proc instr(name: string, impl: Instr): void =
 instr "stop", nil
 
 # Push the instruction pointer, nannified
-instr "here", proc(stack: var seq[uint64], instr_ptr: var uint64): void =
+instr "{", proc(stack: var seq[uint64], instr_ptr: var uint64): void =
   stack.add: instr_ptr.bitor(nan_zero)
   instr_ptr += 1
 
 # Pop the top value, denannify it, and set the instruction pointer to it
-instr "back", proc(stack: var seq[uint64], instr_ptr: var uint64): void =
+instr "}", proc(stack: var seq[uint64], instr_ptr: var uint64): void =
   instr_ptr = stack.pop.bitand(nan_zero.bitnot)
 
 # If the top value is unsigned, advance to the matching `fi`.
 # Otherwise, do nothing.
-instr "if", proc(stack: var seq[uint64], instr_ptr: var uint64): void =
+instr "[", proc(stack: var seq[uint64], instr_ptr: var uint64): void =
 
   if stack.top.sign_bit == 1:
     instr_ptr += 1
@@ -47,9 +47,9 @@ instr "if", proc(stack: var seq[uint64], instr_ptr: var uint64): void =
     var depth = 0
     for idx in instr_ptr ..< stack.len.uint64:
       let instr_code = stack[idx]
-      if instr_code == instr_codes_by_name["if"]:
+      if instr_code == instr_codes_by_name["["]:
         depth += 1
-      elif instr_code == instr_codes_by_name["fi"]:
+      elif instr_code == instr_codes_by_name["]"]:
         depth -= 1
       if depth == 0:
         instr_ptr = idx
@@ -57,7 +57,7 @@ instr "if", proc(stack: var seq[uint64], instr_ptr: var uint64): void =
     abort "nmf"
 
 # Noop
-instr "fi", proc(stack: var seq[uint64], instr_ptr: var uint64): void =
+instr "]", proc(stack: var seq[uint64], instr_ptr: var uint64): void =
   instr_ptr += 1
 
 # Move the next item onto the top of the stack
@@ -76,7 +76,7 @@ instr "dect", proc(stack: var seq[uint64], instr_ptr: var uint64): void =
   instr_ptr += 1
 
 # Flip the sign bit of the top item
-instr "negt", proc(stack: var seq[uint64], instr_ptr: var uint64): void =
+instr "neg", proc(stack: var seq[uint64], instr_ptr: var uint64): void =
   let top = stack.pop
 
   if top.sign_bit == 1:
@@ -87,13 +87,13 @@ instr "negt", proc(stack: var seq[uint64], instr_ptr: var uint64): void =
   instr_ptr += 1
 
 # Rotate the top item one to the right, wrapping
-instr ">>>>", proc(stack: var seq[uint64], instr_ptr: var uint64): void =
+instr "rotr", proc(stack: var seq[uint64], instr_ptr: var uint64): void =
   let top = stack.pop
   stack.add: (top shl 63).bitor(top shr 1)
   instr_ptr += 1
 
 # Rotate the top item one to the left, wrapping
-instr "<<<<", proc(stack: var seq[uint64], instr_ptr: var uint64): void =
+instr "rotl", proc(stack: var seq[uint64], instr_ptr: var uint64): void =
   let top = stack.pop
   stack.add: (top shl 1).bitor(top shr 63)
   instr_ptr += 1
@@ -104,12 +104,12 @@ instr "drop", proc(stack: var seq[uint64], instr_ptr: var uint64): void =
   instr_ptr += 1
 
 # Duplicate the top item of the stack
-instr "dupe", proc(stack: var seq[uint64], instr_ptr: var uint64): void =
+instr "2", proc(stack: var seq[uint64], instr_ptr: var uint64): void =
   stack.add: stack.top
   instr_ptr += 1
 
 # Swap the top two values
-instr "swap", proc(stack: var seq[uint64], instr_ptr: var uint64): void =
+instr "@", proc(stack: var seq[uint64], instr_ptr: var uint64): void =
   stack.add: [stack.pop, stack.pop]  # An obfuscated but cool implementation!
   instr_ptr += 1
 
@@ -133,17 +133,17 @@ instr "read", proc(stack: var seq[uint64], instr_ptr: var uint64): void =
 
 # Print the entire stack
 # Useful for debugging ;)
-instr "dump", proc(stack: var seq[uint64], instr_ptr: var uint64): void =
+instr "show/stack", proc(stack: var seq[uint64], instr_ptr: var uint64): void =
   echo "[" & stack.map(val => cast[float64](val).`$`).join(", ") & "]"
   instr_ptr += 1
 
 # Print the top item as asn ascii character, according to the first 7 bits
-instr "char", proc(stack: var seq[uint64], instr_ptr: var uint64): void =
+instr "show/char", proc(stack: var seq[uint64], instr_ptr: var uint64): void =
   stdout.write stack.top.bitand(0b1111111).char
   instr_ptr += 1
 
 # Print the top item as a bitvector
-instr "bits", proc(stack: var seq[uint64], instr_ptr: var uint64): void =
+instr "show/bits", proc(stack: var seq[uint64], instr_ptr: var uint64): void =
   var chars = ""
   for i in countdown(63, 0):
     chars &= (stack.top shr i).bitand(1).`$`
